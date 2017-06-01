f18972f0ed1326963880f87166a980c6f03fbbbd
Siva <svulise1@kent.edu>
2017-06-01 03:21:34 -0400

    Added MIGRATIONS AND INSERTED VALUES IN DATABASE

---
+++ Migrations/20170601071517_SeedDataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3 ')");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make1-modelA',(SELECT Id FROM Makes WHERE Name='Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make1-modelB',(SELECT Id FROM Makes WHERE Name='Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make1-modelC',(SELECT Id FROM Makes WHERE Name='Make1'))");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make2-modelA',(SELECT Id FROM Makes WHERE Name='Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make2-modelB',(SELECT Id FROM Makes WHERE Name='Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make2-modelC',(SELECT Id FROM Makes WHERE Name='Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make3-modelA',(SELECT Id FROM Makes WHERE Name='Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make3-modelB',(SELECT Id FROM Makes WHERE Name='Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name,MakeId) VALUES ('Make3-modelC',(SELECT Id FROM Makes WHERE Name='Make3'))");
+            
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN('Make1','Make2','Make3')");
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vega.Models
+{
+    [Table("Models")]
+    public class Model
+    {
+        public  int Id { get; set; }
+         [Required]
+        [StringLength(255)]
+        public string Name { get; set; } 
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+        //Here it will not create a new coloumn. we need to follow the naming convention over here.
+        //First name should be the class name and second one should be the property of that class.
+        //Here MakeId act as Foreign key.
+
+        
+    }
+}
